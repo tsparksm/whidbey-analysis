5216eaa4e8c962a74b985391930d0d093cec5fed
Taylor Sparks Martin <taylorific@gmail.com>
2022-09-21 08:57:36 -0700

    Updated .gitignore to include figures; added script to make comparison figures b/w CTD and SSM

---
+++ compare_data_to_ssm.R
@@ -0,0 +1,79 @@
+#### Setup ####
+source(here::here("src", "utility_functions.R"))
+
+# Identify years and scenarios to include
+years <- c(2006, 2008, 2014)
+scenarios <- "Exist1"  # 1 scenario, or length(years) scenarios
+
+#### Load data and information ####
+locator_info <- load_locator_info() %>% 
+  select(Locator, GridCell)
+data_ctd <- load_ctd_data() %>% 
+  mutate(Type = "CTD", 
+         Month = month(Date)) %>% 
+  left_join(locator_info)
+data_discrete <- load_discrete_data() %>% 
+  mutate(Type = "Discrete") %>% 
+  left_join(locator_info)
+data_ssm <- combine_ssm_summary(years, scenarios) %>% 
+  filter(GridCell %in% unique(locator_info$GridCell)) %>% 
+  left_join(locator_info)
+  
+# Create "average" SSM output
+data_ssm_avg <- data_ssm %>% 
+  group_by(Locator, Layer, Month) %>% 
+  summarize(Depth = mean(Depth_m_mean), 
+            DO = mean(DO_mgL_mean), 
+            DO_max = max(DO_mgL_max), 
+            DO_min = min(DO_mgL_min), 
+            NO23 = mean(Nitrate_mgL_mean), 
+            NO23_max = max(Nitrate_mgL_max), 
+            NO23_min = min(Nitrate_mgL_min), 
+            NH3 = mean(Ammonia_mgL_mean), 
+            NH3_max = max(Ammonia_mgL_max), 
+            NH3_min = min(Ammonia_mgL_min), 
+            Chlorophyll = mean(Chla_mean), 
+            Chlorophyll_max = max(Chla_max), 
+            Chlorophyll_min = min(Chla_min)) %>% 
+  mutate(Type = "SSM")
+
+# Combine SSM and CTD output
+data_combined_ctd <- full_join(data_ctd, data_ssm_avg)
+
+# Combine SSM and discrete output ####
+# TO DO
+
+#### Make figure - CTD vs SSM ####
+for (station in unique(locator_info$Locator)) {
+  for (month in 2:8) {
+    ggplot(data = data_combined_ctd %>% 
+             filter(Locator == station, 
+                    Month == month)) + 
+      theme_bw() + 
+      geom_ribbon(aes(x = Depth, 
+                      ymin = DO_min, 
+                      ymax = DO_max, 
+                      group = Type), 
+                  fill = "light gray") + 
+      geom_line(aes(x = Depth, 
+                    y = DO, 
+                    color = Type, 
+                    group = Date)) + 
+      geom_point(aes(x = Depth, 
+                     y = DO, 
+                     shape = Type), 
+                 color = "dark gray") + 
+      scale_shape_manual(values = c(NA, 16)) + 
+      scale_color_manual(values = c("black", "dark gray")) + 
+      coord_flip() + 
+      scale_x_reverse(expand = c(0, 0)) + 
+      labs(x = "Depth (m)", 
+           y = "DO (mg/L)", 
+           color = "", 
+           shape = "", 
+           title = paste(station, "-", month.name[month]))
+    ggsave(filename = here("figs", "ssm", 
+                           paste0(station, "_", month, ".png")), 
+           height = 5, width = 5)
+  }
+}
