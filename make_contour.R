9d334411049cf0c73e423319fdc03ac3fe66d84c
Martin <taymartin@kingcounty.gov>
2023-04-25 15:24:42 -0700

    Changed sigma-t and surface sigma-t plots to include options for all stations to be plotted on one figure

--- make_contour.R
+++ make_contour.R
@@ -4,12 +4,17 @@ library(metR)
 library(cmocean)
 
 # What stations to plot?
-stations <- c("PENNCOVECW", "PENNCOVEENT", "PENNCOVEWEST", 
-              "Poss DO-2", "PSUSANBUOY", "PSUSANENT", 
-              "PSUSANKP", "SARATOGACH", "SARATOGAOP", "SARATOGARP")
+# stations <- c("SARATOGACH", "SARATOGAOP", "SARATOGARP")
+# stations <- c("PSUSANENT", "PSUSANKP", "PSUSANBUOY")
+# stations <- c("PENNCOVECW", "PENNCOVEENT", "PENNCOVEWEST")
+stations <- c("SARATOGACH", "SARATOGAOP", "SARATOGARP",
+              "Poss DO-2", "PSUSANENT", "PSUSANKP", "PSUSANBUOY",
+              "PENNCOVECW", "PENNCOVEENT", "PENNCOVEWEST")
+# stations <- c("Poss DO-2", "PSUSANENT", "PSUSANKP", 
+#               "SARATOGACH", "SARATOGAOP", "SARATOGARP")
 
 # All stations on one figure? TRUE or FALSE
-all_stations_fig <- TRUE
+all_stations_fig <- FALSE
 
 # If all_stations_fig == FALSE, same limits? TRUE or FALSE
 all_stations_lims <- TRUE
@@ -53,61 +58,59 @@ max_depth <- data_ctd %>%
   summarize(MaxDepth = max(BinDepth, na.rm = TRUE)) %>% 
   summarize(MinMaxDepth = min(MaxDepth))
 
-#### Sigma-t contour plot ####
+#### Add extra data before and after each year; filter by MinMaxDepth ####
+data_remix <- data_ctd %>% 
+  filter(Locator %in% stations)
 for (station in stations) {
-  max_depth <- data_ctd %>% 
-    filter(Year <= years[2], 
-           Year >= years[1], 
-           Locator == station) %>% 
-    group_by(Year, YearDay) %>% 
-    summarize(MaxDepth = max(BinDepth, na.rm = TRUE)) %>% 
-    summarize(Value = min(MaxDepth)) %>% 
-    pull(Value)
-  
-  lims <- c(20, 23.6)  # min and max sigma-t values
-  
-  mybreaks <- seq(lims[1], lims[2], by = acc_sigmaT)
-  mylabels <- mybreaks
-  mylabels[!(round(mylabels, 2) == round(round(mylabels, 2)))] <- ""
-  mylabels[1] <- paste0("<", lims[1])
-  mylabels[length(mylabels)] <- paste0(">", lims[2])
-  
-  data_to_plot <- data_ctd %>% 
-    filter(Locator == station, 
+  for (yoi in years[1]:years[2]) {
+    extra_data_before <- data_remix %>% 
+      filter(Year == yoi - 1, 
+             YearDay == max(YearDay), 
+             Locator == station) %>% 
+      mutate(YearDay = YearDay - 365, 
+             Year = yoi)
+      
+    data_remix <- add_row(data_remix, extra_data_before)
+    
+    extra_data_after <- data_remix %>% 
+      filter(Year == yoi + 1, 
+             YearDay == min(YearDay), 
+             Locator == station) %>% 
+      mutate(YearDay = YearDay + 365, 
+             Year = yoi)
+    data_remix <- add_row(data_remix, extra_data_after)
+  }
+}
+
+data_remix <- data_remix %>% 
+  filter(Year %in% years[1]:years[2]) %>% 
+  left_join(max_depth)
+
+#### Sigma-t contour plot ####
+lims <- c(20, 23.6)  # min and max sigma-t values
+mybreaks <- seq(lims[1], lims[2], by = acc_sigmaT)
+mylabels <- mybreaks
+mylabels[!(round(mylabels, 2) == round(round(mylabels, 2)))] <- ""
+mylabels[1] <- paste0("<", lims[1])
+mylabels[length(mylabels)] <- paste0(">", lims[2])
+
+if (all_stations_fig) {
+  data_to_plot <- data_remix %>% 
+    filter(Locator %in% stations, 
            !is.na(SigmaTheta), 
-           BinDepth <= max_depth) %>% 
-    group_by(Year, YearDay, BinDepth) %>% 
+           BinDepth <= MinMaxDepth) %>% 
+    group_by(Locator, Year, YearDay, BinDepth) %>% 
     summarize(SigmaTheta = mean(SigmaTheta, na.rm = TRUE)) %>% 
     ungroup() %>% 
     mutate(SigmaTheta = case_when(SigmaTheta < lims[1] ~ lims[1], 
                                   SigmaTheta > lims[2] ~ lims[2], 
                                   TRUE ~ SigmaTheta))
-  
-  # Add extra data before and after (assuming multiple years of data)
-  for (yoi in years[1]:years[2]) {
-    extra_data_before <- data_to_plot %>% 
-      filter(Year == yoi - 1, 
-             YearDay == max(YearDay)) %>% 
-      mutate(YearDay = YearDay - 365, 
-             Year = yoi)
-    data_to_plot <- add_row(data_to_plot, extra_data_before)
-    
-    extra_data_after <- data_to_plot %>% 
-      filter(Year == yoi + 1, 
-             BinDepth <= max_depth, 
-             YearDay == min(YearDay)) %>% 
-      mutate(YearDay = YearDay + 365, 
-             Year = yoi)
-    data_to_plot <- add_row(data_to_plot, extra_data_after)
-  }
-  
-  data_to_plot <- filter(data_to_plot, Year %in% years[1]:years[2])
   
   ggplot(data = data_to_plot %>% 
            filter(Year %in% years[1]:years[2])) + 
     theme_classic() + 
-    facet_wrap(~ Year, 
-               ncol = 1) + 
+    facet_wrap(~ Locator, ncol = 1, 
+               scales = "free_y") + 
     metR::geom_contour_fill(aes(x = YearDay, 
                                 y = BinDepth, 
                                 z = SigmaTheta), 
@@ -142,72 +145,103 @@ for (station in stations) {
     labs(x = "", 
          y = "Depth (m)", 
          fill = expression(kg/m^3), 
-         title = bquote(.(station)~sigma[Theta]))
+         title = bquote(sigma[Theta]))
   ggsave(here("figs", "contour", "sigmat", 
-              paste0(station, "_sigmat_", 
+              paste0(paste(stations, collapse = "_"), 
+                     "_sigmat_", 
                      years[1], "_", years[2], 
                      ".png")), 
-         height = 2*n, 
+         height = 2*length(stations), 
          width = 8, 
          dpi = 600)
+} else {
+  for (station in stations) {
+    data_to_plot <- data_remix %>% 
+      filter(Locator == station, 
+             !is.na(SigmaTheta), 
+             BinDepth <= MinMaxDepth) %>% 
+      group_by(Year, YearDay, BinDepth) %>% 
+      summarize(SigmaTheta = mean(SigmaTheta, na.rm = TRUE)) %>% 
+      ungroup() %>% 
+      mutate(SigmaTheta = case_when(SigmaTheta < lims[1] ~ lims[1], 
+                                    SigmaTheta > lims[2] ~ lims[2], 
+                                    TRUE ~ SigmaTheta))
+    
+    ggplot(data = data_to_plot %>% 
+             filter(Year %in% years[1]:years[2])) + 
+      theme_classic() + 
+      facet_wrap(~ Year, 
+                 ncol = 1) + 
+      metR::geom_contour_fill(aes(x = YearDay, 
+                                  y = BinDepth, 
+                                  z = SigmaTheta), 
+                              na.fill = TRUE, 
+                              breaks = mybreaks, 
+                              color = alpha("white", sigmat_contour_alpha)) + 
+      scale_fill_cmocean(name = "dense", 
+                         breaks = mybreaks, 
+                         limits = lims, 
+                         labels = mylabels, 
+                         guide = guide_colorbar(show.limits = TRUE, 
+                                                ticks = FALSE, 
+                                                reverse = TRUE)) + 
+      scale_y_reverse(expand = c(0, 0)) + 
+      coord_cartesian(xlim = c(0, 366)) + 
+      scale_x_continuous(expand = c(0, 0), 
+                         breaks = c(yday(paste(yoi, "-01-01", sep = "")), 
+                                    yday(paste(yoi, "-02-01", sep = "")), 
+                                    yday(paste(yoi, "-03-01", sep = "")), 
+                                    yday(paste(yoi, "-04-01", sep = "")), 
+                                    yday(paste(yoi, "-05-01", sep = "")), 
+                                    yday(paste(yoi, "-06-01", sep = "")), 
+                                    yday(paste(yoi, "-07-01", sep = "")), 
+                                    yday(paste(yoi, "-08-01", sep = "")), 
+                                    yday(paste(yoi, "-09-01", sep = "")), 
+                                    yday(paste(yoi, "-10-01", sep = "")), 
+                                    yday(paste(yoi, "-11-01", sep = "")), 
+                                    yday(paste(yoi, "-12-01", sep = ""))), 
+                         labels = month.abb) + 
+      geom_vline(aes(xintercept = YearDay), 
+                 alpha = 0.2) + 
+      labs(x = "", 
+           y = "Depth (m)", 
+           fill = expression(kg/m^3), 
+           title = bquote(.(station)~sigma[Theta]))
+    ggsave(here("figs", "contour", "sigmat", station, 
+                paste0(station, "_sigmat_", 
+                       years[1], "_", years[2], 
+                       ".png")), 
+           height = 2*n, 
+           width = 8, 
+           dpi = 600)
+  }
 }
 
 #### Surface sigma-t contour plot ####
-for (station in stations) {
-  max_depth <- 40
-  # min_lim <- data_ctd %>% 
-  #   filter(Locator == station, 
-  #          !is.na(SigmaTheta), 
-  #          BinDepth <= max_depth) %>% 
-  #   group_by(Year, YearDay) %>% 
-  #   summarize(MinValue = min(SigmaTheta)) %>% 
-  #   summarize(Value = min(MinValue)) %>% 
-  #   pull(Value)
-  
-  # lims <- c(floor(min_lim), 23.6)
-  lims <- c(20, 23.6)  # min and max sigma-t values
-  
-  mybreaks <- seq(lims[1], lims[2], by = acc_sigmaT)
-  mylabels <- mybreaks
-  mylabels[!(round(mylabels, 2) == round(round(mylabels, 2)))] <- ""
-  mylabels[1] <- paste0("<", lims[1])
-  mylabels[length(mylabels)] <- paste0(">", lims[2])
-  
-  data_to_plot <- data_ctd %>% 
-    filter(Locator == station, 
+max_depth <- 40
+lims <- c(20, 23.6)  # min and max sigma-t values
+mybreaks <- seq(lims[1], lims[2], by = acc_sigmaT)
+mylabels <- mybreaks
+mylabels[!(round(mylabels, 2) == round(round(mylabels, 2)))] <- ""
+mylabels[1] <- paste0("<", lims[1])
+mylabels[length(mylabels)] <- paste0(">", lims[2])
+
+if (all_stations_fig) {
+  data_to_plot <- data_remix %>% 
+    filter(Locator %in% stations, 
            !is.na(SigmaTheta), 
            BinDepth <= max_depth) %>% 
-    group_by(Year, YearDay, BinDepth) %>% 
+    group_by(Locator, Year, YearDay, BinDepth) %>% 
     summarize(SigmaTheta = mean(SigmaTheta, na.rm = TRUE)) %>% 
     ungroup() %>% 
     mutate(SigmaTheta = case_when(SigmaTheta < lims[1] ~ lims[1], 
                                   SigmaTheta > lims[2] ~ lims[2], 
                                   TRUE ~ SigmaTheta))
-  
-  # Add extra data before and after (assuming multiple years of data)
-  for (yoi in years[1]:years[2]) {
-    extra_data_before <- data_to_plot %>% 
-      filter(Year == yoi - 1, 
-             YearDay == max(YearDay)) %>% 
-      mutate(YearDay = YearDay - 365, 
-             Year = yoi)
-    data_to_plot <- add_row(data_to_plot, extra_data_before)
-    
-    extra_data_after <- data_to_plot %>% 
-      filter(Year == yoi + 1, 
-             BinDepth <= max_depth, 
-             YearDay == min(YearDay)) %>% 
-      mutate(YearDay = YearDay + 365, 
-             Year = yoi)
-    data_to_plot <- add_row(data_to_plot, extra_data_after)
-  }
-  
-  data_to_plot <- filter(data_to_plot, Year %in% years[1]:years[2])
-  
-  ggplot(data = data_to_plot) + 
+  ggplot(data = data_to_plot %>% 
+           filter(Year %in% years[1]:years[2])) + 
     theme_classic() + 
-    facet_wrap(~ Year, 
-               ncol = 1) + 
+    facet_wrap(~ Locator, ncol = 1, 
+               scales = "free_y") + 
     metR::geom_contour_fill(aes(x = YearDay, 
                                 y = BinDepth, 
                                 z = SigmaTheta), 
@@ -242,14 +276,76 @@ for (station in stations) {
     labs(x = "", 
          y = "Depth (m)", 
          fill = expression(kg/m^3), 
-         title = bquote(.(station)~sigma[Theta]))
+         title = bquote(Surface~sigma[Theta]))
   ggsave(here("figs", "contour", "sigmat", 
-              paste0(station, "_surface_sigmat_", 
+              paste0(paste(stations, collapse = "_"), 
+                     "_surface_sigmat_", 
                      years[1], "_", years[2], 
                      ".png")), 
-         height = 2*n, 
+         height = 2*length(stations), 
          width = 8, 
          dpi = 600)
+} else {
+  for (station in stations) {
+    data_to_plot <- data_remix %>% 
+      filter(Locator == station, 
+             !is.na(SigmaTheta), 
+             BinDepth <= max_depth) %>% 
+      group_by(Year, YearDay, BinDepth) %>% 
+      summarize(SigmaTheta = mean(SigmaTheta, na.rm = TRUE)) %>% 
+      ungroup() %>% 
+      mutate(SigmaTheta = case_when(SigmaTheta < lims[1] ~ lims[1], 
+                                    SigmaTheta > lims[2] ~ lims[2], 
+                                    TRUE ~ SigmaTheta))
+    
+    ggplot(data = data_to_plot %>% 
+             filter(Year %in% years[1]:years[2])) + 
+      theme_classic() + 
+      facet_wrap(~ Year, 
+                 ncol = 1) + 
+      metR::geom_contour_fill(aes(x = YearDay, 
+                                  y = BinDepth, 
+                                  z = SigmaTheta), 
+                              na.fill = TRUE, 
+                              breaks = mybreaks, 
+                              color = alpha("white", sigmat_contour_alpha)) + 
+      scale_fill_cmocean(name = "dense", 
+                         breaks = mybreaks, 
+                         limits = lims, 
+                         labels = mylabels, 
+                         guide = guide_colorbar(show.limits = TRUE, 
+                                                ticks = FALSE, 
+                                                reverse = TRUE)) + 
+      scale_y_reverse(expand = c(0, 0)) + 
+      coord_cartesian(xlim = c(0, 366)) + 
+      scale_x_continuous(expand = c(0, 0), 
+                         breaks = c(yday(paste(yoi, "-01-01", sep = "")), 
+                                    yday(paste(yoi, "-02-01", sep = "")), 
+                                    yday(paste(yoi, "-03-01", sep = "")), 
+                                    yday(paste(yoi, "-04-01", sep = "")), 
+                                    yday(paste(yoi, "-05-01", sep = "")), 
+                                    yday(paste(yoi, "-06-01", sep = "")), 
+                                    yday(paste(yoi, "-07-01", sep = "")), 
+                                    yday(paste(yoi, "-08-01", sep = "")), 
+                                    yday(paste(yoi, "-09-01", sep = "")), 
+                                    yday(paste(yoi, "-10-01", sep = "")), 
+                                    yday(paste(yoi, "-11-01", sep = "")), 
+                                    yday(paste(yoi, "-12-01", sep = ""))), 
+                         labels = month.abb) + 
+      geom_vline(aes(xintercept = YearDay), 
+                 alpha = 0.2) + 
+      labs(x = "", 
+           y = "Depth (m)", 
+           fill = expression(kg/m^3), 
+           title = bquote(.(station)~surface~sigma[Theta]))
+    ggsave(here("figs", "contour", "sigmat", station, 
+                paste0(station, "_surface_sigmat_", 
+                       years[1], "_", years[2], 
+                       ".png")), 
+           height = 2*n, 
+           width = 8, 
+           dpi = 600)
+  }
 }
 
 #### DO contour plot ####
