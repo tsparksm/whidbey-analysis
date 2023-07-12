e3ee26ac873854deb16d216903b0d6db7b888afa
Martin <tsparksmartin@gmail.com>
2023-07-12 11:15:39 -0700

    Added functions to handle coupeville wharf data from Hydrosphere

--- src/utility_functions.R
+++ src/utility_functions.R
@@ -391,3 +391,60 @@ load_climate <- function() {
                             WT08 = col_skip(), 
                             WT08_ATTRIBUTES = col_skip()))
 }
+
+# Load in raw Coupeville mooring output downloaded from Hydrosphere
+# fpath is an optional filepath specification; if blank will pop up file selection
+load_coupeville_hydrosphere <- function(fpath) {
+  if (missing(fpath)) {
+    fpath <- choose.files(caption = "Select file to load", 
+                          multi = FALSE)
+  }
+
+  mooring_data <- read_csv(fpath, 
+                           col_types = cols(
+                             UnixTimestamp = col_double(), 
+                             `Date(America/Los_Angeles)` = col_date(format = 
+                                                                      "%m/%d/%Y"), 
+                             `Time(America/Los_Angeles)` = col_time(), 
+                             SystemBattery = col_double(), 
+                             `HCEP(TEMP)` = col_double(), 
+                             `HCEP(COND)` = col_double(), 
+                             `HCEP(PRES)` = col_double(), 
+                             `HCEP(OXY)` = col_double(), 
+                             `HCEP(PH)` = col_double(), 
+                             `HCEP(FL)` = col_double(), 
+                             `HCEP(TURB)` = col_double(), 
+                             `HCEP(FSD)` = col_double(), 
+                             `HCEP(TSD)` = col_double(), 
+                             `HCEP(SAL)` = col_double(), 
+                             `HCEP(SNDV)` = col_double(), 
+                             `HCEP(SPC)` = col_double(), 
+                             `HCEP(OSAT)` = col_double(), 
+                             `HCEP(VOLT)` = col_double(), 
+                             `HCEP(NUM)` = col_double(), 
+                             `SUNA(M_Parameter1)` = col_double(), 
+                             `SUNA(M_Parameter2)` = col_double(), 
+                             `SUNA(M_Parameter3)` = col_double(), 
+                             `SUNA(M_Parameter4)` = col_double(), 
+                             `HCEP(ERR)` = col_double()
+                           ))
+}
+
+# Take raw hydrosphere output from Coupeville, filter by datetime, and save .csv
+# .csv is in the correct format to upload/append to Socrata
+# Date format should be "YYYY-MM-DD"
+# Time format should be "HH:MM" in 24 hr clock
+process_coupeville_socrata <- function(start_date, 
+                               start_time) {
+  raw_data <- load_coupeville_hydrosphere() %>% 
+    filter(`Date(America/Los_Angeles)` >= start_date, 
+           `Time(America/Los_Angeles)` >= as_hms(paste0(start_time, ":00")))
+  
+  fpath <- choose.files(caption = "Choose .csv file to save", 
+                        multi = FALSE)
+  
+  write_csv(raw_data, 
+            file = fpath)
+}
+
+
