f966b53f70e1d16d9291493ece001aeaa80353f0
Martin <tsparksmartin@gmail.com>
2023-08-17 13:42:40 -0700

    Added map w/ only KC stations

--- make_map.R
+++ make_map.R
@@ -29,6 +29,7 @@ map_transparent <- matrix(adjustcolor(PS_map, alpha.f = 0.2),
                           nrow = nrow(PS_map))
 attributes(map_transparent) <- mapatt
 
+#### Map with all agencies' stations ####
 ggmap(map_transparent) + 
   theme_bw() + 
   theme(panel.grid = element_blank(), 
@@ -44,3 +45,27 @@ ggmap(map_transparent) +
   labs(x = "", y = "", color = "", shape = "")
 ggsave(here("figs", "whidbey_station_map.png"), 
        dpi = 600)
+
+#### Map with just KC stations ####
+ggmap(map_transparent) + 
+  theme_bw() + 
+  theme(panel.grid = element_blank(), 
+        panel.background = element_rect(fill = 'white'), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank(), 
+        panel.border = element_rect(colour = "black", fill=NA, size=1), 
+        legend.position = "none") + 
+  geom_point(data = locators %>% 
+               filter(Agency == "King County") %>% 
+               mutate(Shape = case_when(!is.na(Last_Year) ~ "discontinued", 
+                                        Data_Type == "CTD" ~ "CTD", 
+                                        TRUE ~ "CTD+bottle")), 
+             aes(x = Lon, y = Lat, shape = Shape), 
+             size = 1) + 
+  scale_shape_manual(values = c("discontinued" = 4, 
+                                "CTD" = 17, 
+                                "CTD+bottle" = 16)) + 
+  labs(x = "", y = "", shape = "")
+ggsave(here("figs", "whidbey_station_map_KC.png"), 
+       dpi = 600, 
+       height = 3, width = 2.5)
