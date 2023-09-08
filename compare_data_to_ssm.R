7941cba7e682b6c4e7c7f8432624033b713d0463
Martin <tsparksmartin@gmail.com>
2023-09-08 14:19:23 -0700

    Updated SSM comparison script to use current functions, add new figure with N

--- compare_data_to_ssm.R
+++ compare_data_to_ssm.R
@@ -8,11 +8,12 @@ scenarios <- "Exist1"  # 1 scenario, or length(years) scenarios
 #### Load data and information ####
 locator_info <- load_locator_kc() %>% 
   select(Locator, GridCell)
-data_ctd <- load_ctd_data() %>% 
+data_ctd <- load_composite(bin_size = 0.5, 
+                           monthly = FALSE) %>% 
   mutate(Type = "CTD", 
          Month = month(Date)) %>% 
   left_join(locator_info)
-data_discrete <- load_discrete_data() %>% 
+data_discrete <- load_whidbey_discrete() %>% 
   mutate(Type = "Discrete") %>% 
   left_join(locator_info)
 data_ssm <- combine_ssm_summary(years, scenarios) %>% 
@@ -43,9 +44,9 @@ data_combined_ctd <- full_join(data_ctd, data_ssm_avg)
 # Combine SSM and discrete output ####
 # TO DO
 
-#### Figure - CTD vs SSM ####
+#### Figure - CTD vs SSM DO; all stations, months ####
 for (station in unique(locator_info$Locator)) {
-  for (month in 2:8) {
+  for (month in 1:12) {
     ggplot(data = data_combined_ctd %>% 
              filter(Locator == station, 
                     Month == month)) + 
@@ -72,8 +73,46 @@ for (station in unique(locator_info$Locator)) {
            color = "", 
            shape = "", 
            title = paste(station, "-", month.name[month]))
-    ggsave(filename = here("figs", "ssm", 
-                           paste0(station, "_", month, ".png")), 
+    ggsave(filename = here("figs", "ssm", "do", 
+                           paste0(station, "_", month, "_DO.png")), 
            height = 5, width = 5)
   }
 }
+
+#### Figure - CTD vs SSM N; all stations, months ####
+for (station in unique(locator_info$Locator)) {
+  for (month in 1:12) {
+    ggplot(data = data_combined_ctd %>% 
+             filter(Locator == station, 
+                    Month == month)) + 
+      theme_bw() + 
+      geom_ribbon(aes(x = Depth, 
+                      ymin = NO23_min, 
+                      ymax = NO23_max, 
+                      group = Type), 
+                  fill = "light gray") + 
+      geom_line(aes(x = Depth, 
+                    y = NO23, 
+                    color = Type, 
+                    group = Date)) + 
+      geom_point(aes(x = Depth, 
+                     y = NO23, 
+                     shape = Type), 
+                 color = "dark gray") + 
+      scale_shape_manual(values = c(NA, 16)) + 
+      scale_color_manual(values = c("black", "dark gray")) + 
+      coord_flip() + 
+      scale_x_reverse(expand = c(0, 0)) + 
+      labs(x = "Depth (m)", 
+           y = "Nitrate+nitrite N (mg/L)", 
+           color = "", 
+           shape = "", 
+           title = paste(station, "-", month.name[month]))
+    ggsave(filename = here("figs", "ssm", "no23", 
+                           paste0(station, "_", month, "_NO23.png")), 
+           height = 5, width = 5)
+  }
+}
+
+#### Figure - CTD vs SSM DO and N; one station, few months ####
+
