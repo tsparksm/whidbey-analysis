c7376607c49b862a471ce4d6bf23581428af1b28
Martin <tsparksmartin@gmail.com>
2023-10-25 08:01:37 -0700

    Added Penn Cove data handling functions; phytoplankton group data handling function

--- src/utility_functions.R
+++ src/utility_functions.R
@@ -414,8 +414,8 @@ load_hydrosphere_coupeville <- function(fpath) {
   mooring_data <- read_csv(fpath, 
                            col_types = cols(
                              UnixTimestamp = col_double(), 
-                             `Date(America/Los_Angeles)` = col_date(format = 
-                                                                      "%m/%d/%Y"), 
+                             `Date(America/Los_Angeles)` = col_date(
+                               format = "%m/%d/%Y"), 
                              `Time(America/Los_Angeles)` = col_time(), 
                              SystemBattery = col_double(), 
                              `HCEP(TEMP)` = col_double(), 
@@ -441,6 +441,50 @@ load_hydrosphere_coupeville <- function(fpath) {
                            ))
 }
 
+# Load in raw Penn Cove surface mooring output downloaded from Hydrosphere
+# fpath is an optional filepath specification; if blank will pop up file selection
+load_hydrosphere_penncovesurf <- function(fpath) {
+  if (missing(fpath)) {
+    fpath <- choose.files(caption = "Select file to load", 
+                          multi = FALSE)
+  }
+  
+  mooring_data <- read_csv(fpath, 
+                           col_types = cols(
+                             UnixTimestamp = col_double(), 
+                             `Date(America/Los_Angeles)` = col_date(
+                               format = "%m/%d/%Y"), 
+                             `Time(America/Los_Angeles)` = col_time(), 
+                             WindDirection = col_double(), 
+                             WindSpeed = col_double(), 
+                             SystemBattery = col_double(), 
+                             `HCEP(TEMP)` = col_double(), 
+                             `HCEP(COND)` = col_double(), 
+                             `HCEP(PRES)` = col_double(), 
+                             `HCEP(OXY)` = col_double(), 
+                             `HCEP(PH)` = col_double(), 
+                             `HCEP(FL)` = col_double(), 
+                             `HCEP(TURB)` = col_double(), 
+                             `HCEP(FSD)` = col_double(), 
+                             `HCEP(TSD)` = col_double(), 
+                             `HCEP(SAL)` = col_double(), 
+                             `HCEP(SNDV)` = col_double(), 
+                             `HCEP(SPC)` = col_double(), 
+                             `HCEP(OSAT)` = col_double(), 
+                             `HCEP(VOLT)` = col_double(), 
+                             `HCEP(NUM)` = col_double(), 
+                             `SUNA(M_Parameter1)` = col_double(), 
+                             `SUNA(M_Parameter2)` = col_double(), 
+                             `SUNA(M_Parameter3)` = col_double(), 
+                             `SUNA(M_Parameter4)` = col_double(), 
+                             `HCEP(ERR)` = col_double(), 
+                             `PAR(PAR_SQ-421X)` = col_double(), 
+                             `SDI-12Sensor(M_Parameter1)` = col_double(), 
+                             `SDI-12Sensor(M_Parameter2)` = col_double(), 
+                             `SDI-12Sensor(M_Parameter3)` = col_double()
+                           ))
+}
+
 # Take raw hydrosphere output from Coupeville, filter by datetime, and save .csv
 # .csv is in the correct format to upload/append to Socrata
 # Date format should be "YYYY-MM-DD"
@@ -460,7 +504,27 @@ process_socrata_coupeville <- function(start_date,
             file = fpath)
 }
 
-# Take Coupeville Hydrosphere data and set it up for clean use in R
+# Take raw hydrosphere output from Penn Cove surface, filter by datetime, and save .csv
+# .csv is in the correct format to upload/append to Socrata
+# Date format should be "YYYY-MM-DD"
+# Time format should be "HH:MM" in 24 hr clock
+process_socrata_penncovesurf <- function(start_date, 
+                                       start_time) {
+  raw_data <- load_hydrosphere_penncovesurf() %>% 
+    filter(`Date(America/Los_Angeles)` > start_date | 
+             `Date(America/Los_Angeles)` == start_date & 
+             `Time(America/Los_Angeles)` >= as_hms(paste0(start_time, ":00")))
+  
+  fpath <- here("data", "socrata", paste0("penncovesurf_socrata_", 
+                                          Sys.Date(), 
+                                          ".csv"))
+  
+  write_csv(raw_data, 
+            file = fpath)
+}
+
+
+# Take Coupeville raw Socrata data and set it up for clean use in R
 # Uses static file location, downloaded from Socrata
 load_coupeville <- function() {
   fpath <- here("data", "raw", "Coupeville_Wharf_Mooring_Raw_Data_Output.csv")
@@ -504,6 +568,12 @@ load_coupeville <- function() {
   return(raw_data)
 }
 
+# Take Penn Cove surface raw Socrata data and set it up for clean use in R
+# Uses static file location, downloaded from Socrata
+load_penncovesurf <- function() {
+  # TO DO
+}
+
 # Download marine phytoplankton data (grouped by size class) from Socrata
 get_phyto <- function() {
   phyto_url <- "https://data.kingcounty.gov/resource/ap4k-tvru.csv"
@@ -511,6 +581,14 @@ get_phyto <- function() {
     rename(total_abundance = total_abundance_particles, 
            total_biovolume = total_biovolume_mm_3_l) %>% 
     select(locator:total_biovolume)
+}
+
+get_phyto_group <- function() {
+  phyto_url <- "https://data.kingcounty.gov/resource/xyty-wq8n.csv"
+  phyto_data <- read.socrata(phyto_url) %>% 
+    rename(biovolume = biovolume_mm_3_l, 
+           abundance = abundance_particles_ml) %>% 
+    select(locator:quality)
 }
 
 load_EIM_continuous <- function(station) {
