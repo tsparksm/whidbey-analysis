0903ad8416d9c285f5f6e3ca2a1baa3a0ee17e68
Martin <tsparksmartin@gmail.com>
2023-10-25 08:01:53 -0700

    Added combo biovolume and nuts/chl figures

--- phytoplankton.R
+++ phytoplankton.R
@@ -3,6 +3,8 @@ source(here::here("src", "utility_functions.R"))
 library(RSocrata)
 
 phyto_data <- get_phyto()
+group_data <- get_phyto_group()
+discrete_data <- load_whidbey_discrete()
 
 whidbey_stations <- c(
   "PENNCOVEENT", 
@@ -56,3 +58,38 @@ ggplot(data_to_plot,
 ggsave(here("figs", "phyto", 
             paste0("Central_vs_Whidbey_biovolume_", yoi, ".png")), 
        dpi = 600, height = 4, width = 6)
+
+#### Biovolume and chlorophyll ####
+temp <-  phyto_data %>% 
+  select(c(sample_number, total_abundance, total_biovolume)) %>% 
+  rename(LabSampleNum = sample_number)
+combined_data <- discrete_data %>% 
+  filter(ParmId == 1) %>% 
+  left_join(temp)
+
+ggplot(combined_data %>% 
+         filter(DepthBin == "surface"), 
+       aes(x = CollectDate)) + 
+  geom_point(aes(y = Value)) + 
+  geom_point(aes(y = total_biovolume), color = "gray")
+
+ggplot(combined_data %>% 
+         filter(DepthBin == "surface"), 
+       aes(x = Value, y = total_biovolume)) + 
+  geom_point()
+
+#### Biovolume and nutrients ####
+temp <-  phyto_data %>% 
+  select(c(sample_number, total_abundance, total_biovolume)) %>% 
+  rename(LabSampleNum = sample_number)
+combined_data <- discrete_data %>% 
+  filter(ParmId == 14) %>% 
+  left_join(temp)
+
+ggplot(combined_data %>% 
+         filter(DepthBin == "surface"), 
+       aes(x = total_biovolume, 
+           y = Value, 
+           color = as.factor(month(CollectDate)))) + 
+  geom_point() + 
+  facet_wrap(~ Locator)
