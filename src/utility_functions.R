9f793d626269d089e1e467ff74b9db0caff735e0
Martin <tsparksmartin@gmail.com>
2024-01-04 13:02:23 -0800

    Added Port Susan buoy data handling functions

--- src/utility_functions.R
+++ src/utility_functions.R
@@ -485,6 +485,50 @@ load_hydrosphere_penncovesurf <- function(fpath) {
                            ))
 }
 
+# Load in raw Port Susan buoy output downloaded from Hydrosphere
+# fpath is an optional filepath specification; if blank will pop up file selection
+load_hydrosphere_psusan <- function(fpath) {
+  if (missing(fpath)) {
+    fpath <- choose.files(caption = "Select file to load", 
+                          multi = FALSE)
+  }
+  
+  mooring_data <- read_csv(fpath, 
+                           col_types = cols(
+                             UnixTimestamp = col_double(), 
+                             `Date(America/Los_Angeles)` = col_date(
+                               format = "%m/%d/%Y"), 
+                             `Time(America/Los_Angeles)` = col_time(), 
+                             WindDirection = col_double(), 
+                             WindSpeed = col_double(), 
+                             SystemBattery = col_double(), 
+                             `HCEP(TEMP)` = col_double(), 
+                             `HCEP(COND)` = col_double(), 
+                             `HCEP(PRES)` = col_double(), 
+                             `HCEP(OXY)` = col_double(), 
+                             `HCEP(PH)` = col_double(), 
+                             `HCEP(FL)` = col_double(), 
+                             `HCEP(TURB)` = col_double(), 
+                             `HCEP(FSD)` = col_double(), 
+                             `HCEP(TSD)` = col_double(), 
+                             `HCEP(SAL)` = col_double(), 
+                             `HCEP(SNDV)` = col_double(), 
+                             `HCEP(SPC)` = col_double(), 
+                             `HCEP(OSAT)` = col_double(), 
+                             `HCEP(VOLT)` = col_double(), 
+                             `HCEP(NUM)` = col_double(), 
+                             `SUNA(M_Parameter1)` = col_double(), 
+                             `SUNA(M_Parameter2)` = col_double(), 
+                             `SUNA(M_Parameter3)` = col_double(), 
+                             `SUNA(M_Parameter4)` = col_double(), 
+                             `HCEP(ERR)` = col_double(), 
+                             `PAR(PAR_SQ-421X)` = col_double(), 
+                             `WTX534(Temp)` = col_double(), 
+                             `WTX534(Humidity)` = col_double(), 
+                             `WTX534(Pressure)` = col_double()
+                           ))
+}
+
 # Take raw hydrosphere output from Coupeville, filter by datetime, and save .csv
 # .csv is in the correct format to upload/append to Socrata
 # Date format should be "YYYY-MM-DD"
@@ -523,6 +567,24 @@ process_socrata_penncovesurf <- function(start_date,
             file = fpath)
 }
 
+# Take raw hydrosphere output from Port Susan buoy, filter by datetime, and save .csv
+# .csv is in the correct format to upload/append to Socrata
+# Date format should be "YYYY-MM-DD"
+# Time format should be "HH:MM" in 24 hr clock
+process_socrata_psusan <- function(start_date, 
+                                         start_time) {
+  raw_data <- load_hydrosphere_psusan() %>% 
+    filter(`Date(America/Los_Angeles)` > start_date | 
+             `Date(America/Los_Angeles)` == start_date & 
+             `Time(America/Los_Angeles)` >= as_hms(paste0(start_time, ":00")))
+  
+  fpath <- here("data", "socrata", paste0("psusan_socrata_", 
+                                          Sys.Date(), 
+                                          ".csv"))
+  
+  write_csv(raw_data, 
+            file = fpath)
+}
 
 # Take Coupeville raw Socrata data and set it up for clean use in R
 # Uses static file location, downloaded from Socrata
@@ -571,6 +633,12 @@ load_coupeville <- function() {
 # Take Penn Cove surface raw Socrata data and set it up for clean use in R
 # Uses static file location, downloaded from Socrata
 load_penncovesurf <- function() {
+  # TO DO
+}
+
+# Take Port Susan buoy raw Socrata data and set it up for clean use in R
+# Uses static file location, downloaded from Socrata
+load_psusan <- function() {
   # TO DO
 }
 
