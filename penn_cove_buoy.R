710c40bb4f52fa30a9805298a1ce8499758b0609
Martin <tsparksmartin@gmail.com>
2024-04-10 14:01:42 -0700

    Remove Socrata file handling - Penn Cove

--- penn_cove_buoy.R
+++ penn_cove_buoy.R
@@ -4,48 +4,6 @@ library(rtide)
 
 yoi <- 2023
 
-#### Create file for import in Socrata - surface ####
-# Will use GUI for load and save file selection
-start_time <- "11:30"  # 24 hr HH:MM
-start_date <- "2024-01-04"  # YYYY-MM-DD
-process_socrata_penncovesurf(start_date, start_time)
-
-#### Combine SUNA and HCEP data - bottom ####
-data_HCEP <- load_PC_bottom_HCEP()
-data_SUNA <- load_PC_bottom_SUNA()
-
-data_SUNA_processed <- data_SUNA %>% 
-  group_by(SUNA_id, DateTime) %>% 
-  summarize(NO3_umol = mean(NO3_umol, na.rm = T), 
-            NO3_mgNL = mean(NO3_mgNL, na.rm = T), 
-            NO3_n = n()) %>% 
-  mutate(DateTime = with_tz(DateTime, tzone = "Etc/GMT+8"))
-
-data_HCEP_1 <- data_HCEP %>% 
-  filter(row_number() <= 3616)
-tz(data_HCEP_1$Datetime) <- "Etc/GMT+7"
-data_HCEP_1 <- data_HCEP_1 %>% 
-  mutate(DateTime = with_tz(Datetime, tzone = "Etc/GMT+8"))
-data_HCEP_2 <- data_HCEP %>% 
-  filter(row_number() > 3616) %>% 
-  mutate(DateTime = Datetime)
-tz(data_HCEP_2$DateTime) <- "Etc/GMT+8"
-data_HCEP_processed <- data_HCEP_1 %>% 
-  add_row(data_HCEP_2) %>% 
-  relocate(DateTime, .after = HCEP_id) %>% 
-  select(-Datetime)
-
-data_combined <- full_join(data_HCEP_processed, data_SUNA_processed) %>% 
-  arrange(DateTime) %>% 
-  filter(row_number() >= 4, 
-         row_number() <= n() - 3)
-
-fpath <- here("data", "socrata", paste0("penncovebottom_socrata_", 
-                                        Sys.Date(), 
-                                        ".csv"))
-
-write_csv(data_combined, fpath)
-
 #### Load Socrata data and combine ####
 surf_data <- load_penncovesurf() %>% 
   rename_with(~str_c(., "_surface"), .cols = Temperature:NO23)
