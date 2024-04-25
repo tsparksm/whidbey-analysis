2361ef43dc3ffdd25d57cbc34c0694bb0df54386
Martin <tsparksmartin@gmail.com>
2024-04-25 16:22:32 -0700

    Added Penn Cove buoy no23 and chl figures

--- penn_cove_buoy.R
+++ penn_cove_buoy.R
@@ -16,13 +16,15 @@ discrete_data <- load_whidbey_discrete()
 ctd_data <- load_CTD("PENNCOVEENT")
 
 #### Figure: year of DO, top vs bottom ####
+yoi <- 2023
 data_to_plot <- combo_data %>% 
   select(DateTime, Oxygen_mgL_bottom, Oxygen_surface) %>% 
   rename(Bottom = Oxygen_mgL_bottom, 
          Surface = Oxygen_surface) %>% 
   pivot_longer(cols = !DateTime, 
                names_to = "Location", 
-               values_to = "DO")
+               values_to = "DO") %>% 
+  filter(year(DateTime) == yoi)
 ggplot(data = data_to_plot, 
        aes(x = DateTime, 
            y = DO, 
@@ -34,7 +36,65 @@ ggplot(data = data_to_plot,
   scale_color_brewer(palette = "Paired") + 
   labs(x = "", 
        y = "Oxygen (mg/L)", 
-       title = "Penn Cove", 
+       title = paste(yoi, "Penn Cove"),  
        color = "")
-ggsave(here("figs", "penncove", "surface_vs_bottom_do.png"), 
+ggsave(here("figs", "penncove", 
+            paste(yoi, "_surface_vs_bottom_do.png")), 
        height = 6, width = 10)
+
+#### Figure: year of NO23, top vs bottom ####
+yoi <- 2023
+data_to_plot <- combo_data %>% 
+  select(DateTime, NO3_mgNL_bottom, NO23_surface) %>% 
+  rename(Bottom = NO3_mgNL_bottom, 
+         Surface = NO23_surface) %>% 
+  pivot_longer(cols = !DateTime, 
+               names_to = "Location", 
+               values_to = "NO23") %>% 
+  filter(year(DateTime) == yoi, 
+         NO23 >= 0)
+ggplot(data = data_to_plot, 
+       aes(x = DateTime, 
+           y = NO23, 
+           color = Location)) + 
+  theme_bw() + 
+  theme(text = element_text(size = 20)) + 
+  geom_point() + 
+  # scale_y_continuous(limits = c(0, 15)) + 
+  scale_color_brewer(palette = "Paired") + 
+  labs(x = "", 
+       y = "Nitrate+nitrite (mg N/L)", 
+       title = paste(yoi, "Penn Cove"),  
+       color = "")
+ggsave(here("figs", "penncove", 
+            paste(yoi, "_surface_vs_bottom_no23.png")), 
+       height = 6, width = 10)
+
+#### Figure: year of chl, top vs bottom ####
+yoi <- 2023
+data_to_plot <- combo_data %>% 
+  select(DateTime, Chlorophyll_ugL_bottom, Chlorophyll_surface) %>% 
+  rename(Bottom = Chlorophyll_ugL_bottom, 
+         Surface = Chlorophyll_surface) %>% 
+  pivot_longer(cols = !DateTime, 
+               names_to = "Location", 
+               values_to = "Chlorophyll") %>% 
+  filter(year(DateTime) == yoi, 
+         Chlorophyll > 0)
+ggplot(data = data_to_plot, 
+       aes(x = DateTime, 
+           y = Chlorophyll, 
+           color = Location)) + 
+  theme_bw() + 
+  theme(text = element_text(size = 20)) + 
+  geom_point() + 
+  # scale_y_continuous(limits = c(0, 15)) + 
+  scale_color_brewer(palette = "Paired") + 
+  labs(x = "", 
+       y = expression(Chlorophyll~(mu*g/L)), 
+       title = paste(yoi, "Penn Cove"),  
+       color = "")
+ggsave(here("figs", "penncove", 
+            paste(yoi, "_surface_vs_bottom_chl.png")), 
+       height = 6, width = 10)
+
