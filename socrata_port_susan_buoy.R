7fbc3a48af556b6307d692e56ccbcb1cce4c35e9
Martin <tsparksmartin@gmail.com>
2024-11-07 11:36:14 -0800

    Adjust datetime handling and fix timezone problems in Socrat Port Susan buoy script

--- socrata_port_susan_buoy.R
+++ socrata_port_susan_buoy.R
@@ -12,9 +12,8 @@ load_hydrosphere_psusan <- function(fpath) {
   mooring_data <- read_csv(fpath, 
                            col_types = cols(
                              UnixTimestamp = col_double(), 
-                             `Date(America/Los_Angeles)` = col_date(
-                               format = "%m/%d/%Y"), 
-                             `Time(America/Los_Angeles)` = col_time(), 
+                             Date = col_date(), 
+                             Time = col_time(), 
                              WindDirection = col_double(), 
                              WindSpeed = col_double(), 
                              SystemBattery = col_double(), 
@@ -52,9 +51,15 @@ load_hydrosphere_psusan <- function(fpath) {
 process_socrata_psusan <- function(start_date, 
                                    start_time) {
   raw_data <- load_hydrosphere_psusan() %>% 
-    filter(`Date(America/Los_Angeles)` > start_date | 
-             `Date(America/Los_Angeles)` == start_date & 
-             `Time(America/Los_Angeles)` >= as_hms(paste0(start_time, ":00")))
+    mutate(DateTime = as.POSIXct(paste(Date, Time), 
+                                 tz = "UTC"), 
+           NewDateTime = with_tz(DateTime, tzone = "Etc/GMT+8"), 
+           Date = as.Date(str_sub(NewDateTime, 1, 10)), 
+           Time = parse_time(str_sub(NewDateTime, 12, 16))) %>% 
+    select(-DateTime, -NewDateTime) %>% 
+    filter(Date > start_date | 
+             Date == start_date & 
+             Time >= as_hms(paste0(start_time, ":00")))
   
   fpath <- here("data", "socrata", paste0("psusan_socrata_", 
                                           Sys.Date(), 
@@ -72,3 +77,60 @@ start_date <- str_sub(start_dt, 1, 10)
 process_socrata_psusan(start_date, start_time)
 
 #### Initial Hydrosphere download #### 
+fpath <- here("data", "raw", "Initial Hydrosphere download", 
+              "Port Susan Buoy.csv")
+
+mooring_data <- read_csv(fpath, 
+                         col_types = cols(
+                           UnixTimestamp = col_double(), 
+                           `Date(America/Los_Angeles)` = col_date(
+                             format = "%m/%d/%Y"), 
+                           `Time(America/Los_Angeles)` = col_time(), 
+                           WindDirection = col_double(), 
+                           WindSpeed = col_double(), 
+                           SystemBattery = col_double(), 
+                           `HCEP(TEMP)` = col_double(), 
+                           `HCEP(COND)` = col_double(), 
+                           `HCEP(PRES)` = col_double(), 
+                           `HCEP(OXY)` = col_double(), 
+                           `HCEP(PH)` = col_double(), 
+                           `HCEP(FL)` = col_double(), 
+                           `HCEP(TURB)` = col_double(), 
+                           `HCEP(FSD)` = col_double(), 
+                           `HCEP(TSD)` = col_double(), 
+                           `HCEP(SAL)` = col_double(), 
+                           `HCEP(SNDV)` = col_double(), 
+                           `HCEP(SPC)` = col_double(), 
+                           `HCEP(OSAT)` = col_double(), 
+                           `HCEP(VOLT)` = col_double(), 
+                           `HCEP(NUM)` = col_double(), 
+                           `SUNA(M_Parameter1)` = col_double(), 
+                           `SUNA(M_Parameter2)` = col_double(), 
+                           `SUNA(M_Parameter3)` = col_double(), 
+                           `SUNA(M_Parameter4)` = col_double(), 
+                           `HCEP(ERR)` = col_double(), 
+                           `PAR(PAR_SQ-421X)` = col_double(), 
+                           `WTX534(Temp)` = col_double(), 
+                           `WTX534(Humidity)` = col_double(), 
+                           `WTX534(Pressure)` = col_double()
+                         ))
+processed_data <- mooring_data %>% 
+  rename(Date = `Date(America/Los_Angeles)`, 
+         Time = `Time(America/Los_Angeles)`) %>% 
+  mutate(rn = row_number(), 
+         DateTime = as.POSIXct(paste(Date, Time), tz = "UTC"), 
+         NewTime = case_when(
+           between(rn, 1452, 9197) ~ DateTime + 1*60*60, 
+           TRUE ~ DateTime), 
+         Date = str_sub(as.character(NewTime), 1, 10), 
+         Time = str_sub(as.character(NewTime), 12, 16)
+  ) %>% 
+  filter(rn >= 1452) %>% 
+  select(-NewTime, -DateTime, -rn)
+# select(-DateTime) %>% 
+# rename(DateTime = NewTime) %>% 
+# mutate(DateTime = force_tz(DateTime, tzone = "Etc/GMT-8"))
+
+fpath <- here("data", "socrata", "portsusan_socrata_initial.csv")
+
+write_csv(processed_data, file = fpath)
