06eed3344b5cd76101f79a03f58691af63e052b2
Martin <tsparksmartin@gmail.com>
2024-12-02 14:02:32 -0800

    Added Penn Cove bottom data compilation for 2024-07 to 2024-10

--- socrata_penn_cove_buoy_bottom.R
+++ socrata_penn_cove_buoy_bottom.R
@@ -1,6 +1,105 @@
 #### SETUP ####
 source(here::here("src", "utility_functions.R"))
 
+#### 2024-07 to 2024-10 ####
+fpath_HCEP <- here("data", "raw", "Penn Cove near-bottom", 
+                   "Penn Cove NB HCEP 2024-07 to 2024-10.xlsx")
+data_HCEP <- read_xlsx(
+  fpath_HCEP, 
+  sheet = "Data", 
+  col_types = c(
+    "text", 
+    "date", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "numeric", 
+    "text", 
+    "text", 
+    "numeric"
+  )
+) %>% 
+  rename(
+    HCEP_id = FrameSync, 
+    DateTime = `DateTime (UTC)`, 
+    Temperature_C = `Temperature (Celsius)`, 
+    Conductivity_Sm = `Conductivity (S/m)`, 
+    Pressure_dbar = `Pressure (Decibar)`, 
+    Oxygen_mgL = `Oxygen (mg/L)`, 
+    pH = `pH (pH)`, 
+    Chlorophyll_ugL = `Chlorophyll (ug/l)`, 
+    Turbidity_NTU = `Turbidity (NTU)`, 
+    Chlorophyll_SD_ugL = `Chlorophyll StdDev (ug/l)`, 
+    Turbidity_SD_ugL = `Turbidity StdDev (NTU)`, 
+    Salinity_PSU = `Salinity (psu)`, 
+    Spec_Conductivity_Sm = `Spec Conductivity (S/m)`, 
+    Oxygen_sat = `Oxygen Sat (%)`, 
+    Time = `Time (PST)`, 
+    Event_Flags = `Event Flags`
+  ) %>% 
+  mutate(DateTime = as.POSIXct(DateTime, tz = "UTC") - 8*60*60) %>% 
+  select(-Date, -Time)
+
+fpath_SUNA <- here("data", "raw", "Penn Cove near-bottom", 
+                   "Penn Cove NB SUNA 2024-07 to 2024-10.xlsx")
+data_SUNA <- read_xlsx(
+  fpath_SUNA, 
+  sheet = "SUNA1962_BATCH_pro", 
+  col_types = c(
+    "text", 
+    "date", 
+    "date", 
+    "skip", 
+    "skip", 
+    "skip", 
+    "skip", 
+    "numeric", 
+    "numeric", 
+    "skip", 
+    "skip", 
+    "skip", 
+    "skip", 
+    "skip", 
+    "skip", 
+    "skip"
+  ), 
+  skip = 7
+) %>% 
+  rename(
+    SUNA_id = `Frame Header`, 
+    NO3_umol = `Processed NO3(uMol)`, 
+    NO3_mgNL = `Processed NO3(mg N/L)`
+  ) %>% 
+  mutate(across(NO3_umol:NO3_mgNL, 
+                ~replace(., . == "NaN", NA)), 
+         Time = str_sub(as.character(Time), start = 12), 
+         DateTime = round_date(as.POSIXct(paste(Date, Time), 
+                                          tz = "UTC"), 
+                               "15 mins")) %>% 
+  group_by(SUNA_id, DateTime) %>% 
+  summarize(NO3_umol = mean(NO3_umol, na.rm = T), 
+            NO3_mgNL = mean(NO3_mgNL, na.rm = T), 
+            NO3_n = n()) %>% 
+  mutate(DateTime = DateTime - 8*60*60)
+
+data_combined <- full_join(data_HCEP, data_SUNA) %>% 
+  arrange(DateTime) %>% 
+  filter(row_number() >= 7, 
+         row_number() <= n() - 2) %>% 
+  mutate(DateTime = as.character(DateTime))
+
+fpath <- here("data", "socrata", 
+              "penncovebottom_socrata_2024-12-02.csv")
+write_csv(data_combined, fpath)
+
 #### 2024-05 to 2024-07 ####
 fpath_HCEP <- here("data", "raw", "Penn Cove near-bottom", 
                    "Penn Cove NB HCEP 2024-05 to 2024-07.xlsx")
