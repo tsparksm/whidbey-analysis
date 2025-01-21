990e621174bdc09d495846d264f86d6ebf6870cf
Martin <tsparksmartin@gmail.com>
2025-01-21 11:10:36 -0800

    Coupeville QC - add autoflags and save output

--- qc_coupeville.R
+++ qc_coupeville.R
@@ -24,6 +24,138 @@ tide_data <- tide_height(stations = "Seattle",
                          tz = "Etc/GMT-8")
 comb_data <- left_join(raw_data, tide_data %>% select(-Station))
 
+#### QC auto - gross + site range ####
+qc_data <- raw_data %>% 
+  mutate(
+    Temperature_auto = case_when(
+      !between(Temperature, -5, 45) ~ "g", 
+      !between(Temperature, 0, 21) ~ "r", 
+      TRUE ~ NA
+    ), 
+    Pressure_auto = case_when(
+      !between(Pressure, 0, 10) ~ "g", 
+      Pressure > 6 ~ "r", 
+      TRUE ~ NA
+    ), 
+    Oxygen_auto = case_when(
+      !between(Oxygen, 0, 25) ~ "g", 
+      Oxygen > 22 ~ "r", 
+      TRUE ~ NA
+    ), 
+    pH_auto = case_when(
+      !between(pH, 0, 14) ~ "g", 
+      !between(pH, 7, 9) ~ "r", 
+      TRUE ~ NA
+    ), 
+    Chlorophyll_auto = case_when(
+      !between(Chlorophyll, 0, 400) ~ "g", 
+      Chlorophyll > 200 ~ "r", 
+      TRUE ~ NA
+    ), 
+    Turbidity_auto = case_when(
+      !between(Turbidity, 0, 3000) ~ "g", 
+      Turbidity > 200 ~ "r", 
+      TRUE ~ NA
+    ), 
+    Salinity_auto = case_when(
+      !between(Salinity, 0, 42) ~ "g", 
+      !between(Salinity, 9, 32) ~ "r", 
+      TRUE ~ NA
+    ), 
+    NO23_auto = case_when(
+      !between(NO23, 0.007, 56) ~ "g", 
+      NO23 > 0.5 ~ "r", 
+      TRUE ~ NA
+    )
+  )
+
+#### QC auto - envelope (TEST) ####
+# # Testing just NO23 here
+# w <- 21
+# m <- 3
+# envelope <- qc_data %>% 
+#   arrange(DateTime) %>% 
+#   mutate(
+#     NO23 = ifelse(
+#       is.na(NO23_auto), NO23, NA
+#     ), 
+#     MA = rollapply(NO23, w, mean, 
+#                    na.rm = TRUE, 
+#                    partial = TRUE, 
+#                    fill = NA), 
+#     SD = rollapply(NO23, w, sd, 
+#                    na.rm = TRUE, 
+#                    partial = TRUE, 
+#                    fill = NA)
+#   )
+# 
+# ggplot(envelope %>% 
+#          filter(
+#            between(Date, 
+#                    as.Date("2023-01-01"), 
+#                    as.Date("2023-05-01")
+#            )
+#          ), 
+#        aes(x = DateTime)) + 
+#   theme_bw() + 
+#   geom_ribbon(aes(ymin = MA - m*SD, 
+#                   ymax = MA + m*SD), 
+#               color = "gray", 
+#               fill = "gray") + 
+#   geom_point(aes(y = NO23, 
+#                  color = between(NO23, MA - m*SD, MA + m*SD))) + 
+#   theme(legend.position = "none")
+
+#### QC manual - load/apply spreadsheet of flagged values ####
+manual_qc <- read_xlsx(
+  here("data", "whidbey mooring qc.xlsx"), 
+  sheet = "Coupeville"
+) %>% 
+  mutate(End = if_else(is.na(End), Start, End), 
+         Span = paste(Start, End))
+
+all_dts <- manual_qc %>% 
+  select(Start, End, Span) %>% 
+  group_by(Span) %>% 
+  group_modify(~ tibble(DateTime = seq(.$Start, .$End, by = "15 min")))
+
+all_manual <- full_join(manual_qc, all_dts) %>% 
+  select(DateTime, Pressure:NO23) %>% 
+  mutate(
+    DateTime = force_tz(DateTime, "Etc/GMT+8"), 
+    across(
+      Pressure:NO23, 
+      ~ case_when(
+        str_detect(.x, "q") ~ "s", 
+        str_detect(.x, "endcheck") ~ "f", 
+        !is.na(.x) ~ "b", 
+        TRUE ~ NA
+      )
+    )
+  ) %>% 
+  rename_with(~str_c(., "_manual"), .cols = Pressure:NO23)
+
+qc_data <- left_join(qc_data, all_manual)
+
+#### QC final - determine final quality and save file ####
+# Make sure to include osat quality as a function of o + t quality
+qc_final <- qc_data %>% 
+  relocate(OxygenSat, .after = NO23) %>% 
+  mutate(
+    across(
+      Temperature:NO23, 
+      ~ case_when(
+        pick(paste0(cur_column(), "_auto")) == "g" ~ 3, 
+        pick(paste0(cur_column(), "_manual")) == "b" ~ 3,
+        pick(paste0(cur_column(), "_auto")) == "r" ~ 2,
+        pick(paste0(cur_column(), "_manual")) == "f" ~ 2,
+        pick(paste0(cur_column(), "_manual")) == "s" ~ 2,
+        TRUE ~ 1
+      ), 
+      .names = "{.col}_final"
+    )
+  )
+
 #### FIGURE - pressure ####
 start_date <- as.Date("2023-12-01")
 end_date <- as.Date("2024-01-01")
