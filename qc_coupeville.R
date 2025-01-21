8e994f573972af1687ffef9e2cdf0abd583f0c74
Martin <tsparksmartin@gmail.com>
2025-01-21 13:23:45 -0800

    Coupeville QC - add quality flag colors to chlorophyll QC figure

--- qc_coupeville.R
+++ qc_coupeville.R
@@ -273,10 +273,16 @@ data_to_plot <- comb_data %>%
 
 p <- ggplot(data_to_plot, 
             aes(x = DateTime, 
-                y = Chlorophyll)) + 
-  geom_point(aes(), 
-             size = 0.2) + 
-  theme_bw()
+                y = Chlorophyll, 
+                color = as.factor(Chlorophyll_final))) + 
+  geom_point(size = 0.2) + 
+  scale_color_manual(values = c("1" = "black", 
+                                "2" = "orange", 
+                                "3" = "red")) + 
+  theme_bw() + 
+  theme(legend.position = "none") + 
+  labs(x = "", 
+       y = "Chlorophyll (ug/L)")
 ggplotly(p)
 
 #### FIGURE - turbidity ####
