033477a5495dfa8d64ba1974185af443a608dc0a
Martin <tsparksmartin@gmail.com>
2025-04-23 10:26:15 -0700

    Switch to QC'd data for Coupeville figs

--- coupeville.R
+++ coupeville.R
@@ -6,6 +6,7 @@ yoi <- 2024
 
 #### Load data ####
 # Remember to update file via Socrata download first!
+qc_data <- load_qc_coupeville()
 raw_data <- load_coupeville()
 
 # Remember to update discrete data first!
@@ -17,14 +18,19 @@ ctd_data <- load_CTD("PENNCOVECW")
 #### Load/generate tide data ####
 tide_data <- tide_height(stations = "Seattle", 
                          minutes = 15, 
-                         from = min(raw_data$Date) - 1, 
-                         to = max(raw_data$Date) + 1, 
+                         from = min(qc_data$Date) - 1, 
+                         to = max(qc_data$Date) + 1, 
                          tz = "Etc/GMT-8")
-comb_data <- left_join(raw_data, tide_data %>% select(-Station))
+comb_data <- left_join(qc_data, tide_data %>% select(-Station))
 
 #### Mooring - temperature salinity plot ####
-ggplot(data = raw_data %>% 
-         filter(Year == yoi), 
+data_to_plot <- qc_data %>%  
+  filter(Year == yoi) %>% 
+  mutate(Temperature = ifelse(Temperature_final == 1, 
+                              Temperature, NA), 
+         Salinity = ifelse(Salinity_final == 1, 
+                           Salinity, NA))
+ggplot(data = data_to_plot, 
        aes(x = Temperature, 
            y = Salinity, 
            color = as.factor(Month))) + 
@@ -35,15 +41,16 @@ ggplot(data = raw_data %>%
   labs(x = expression("Temperature " ( degree*C)), 
        y = "Salinity (PSU)", 
        color = "Month", 
-       title = paste("Coupeville", yoi)) + 
-  lims(x = c(4, 22), 
-       y = c(0, 30))
+       title = paste("Coupeville", yoi))
 ggsave(here("figs", "coupeville", paste0("T_S_", yoi, ".png")), 
        height = 4, width = 6)
 
 #### Mooring - temperature over time ####
-ggplot(data = raw_data %>% 
-         filter(Year <= yoi), 
+data_to_plot <- qc_data %>%  
+  filter(Year <= yoi) %>% 
+  mutate(Temperature = ifelse(Temperature_final == 1, 
+                              Temperature, NA))
+ggplot(data = data_to_plot, 
        aes(x = FakeDateTime, 
            y = Temperature, 
            color = Year == yoi)) + 
@@ -63,8 +70,11 @@ ggsave(here("figs", "coupeville", paste0("T_", yoi, ".png")),
        height = 4, width = 6)
 
 #### Mooring - salinity over time ####
-ggplot(data = raw_data %>% 
-         filter(Year <= yoi), 
+data_to_plot <- qc_data %>%  
+  filter(Year <= yoi) %>% 
+  mutate(Salinity = ifelse(Salinity_final == 1, 
+                           Salinity, NA))
+ggplot(data = data_to_plot, 
        aes(x = FakeDateTime, 
            y = Salinity, 
            color = Year == yoi)) + 
@@ -78,16 +88,17 @@ ggplot(data = raw_data %>%
   scale_color_manual(values = c("TRUE" = "black", 
                                 "FALSE" = "gray")) + 
   scale_x_datetime(date_breaks = "1 month", 
-                   date_labels = "%b") + 
-  lims(y = c(0, 31))
+                   date_labels = "%b")
 ggsave(here("figs", "coupeville", paste0("S_", yoi, ".png")), 
        height = 4, width = 6)
 
 #### Mooring - salinity short period ####
 beg_date <- as.Date("2024-11-01")
 end_date <- as.Date("2024-11-30")
-ggplot(data = raw_data %>% 
-         filter(between(Date, beg_date, end_date)), 
+ggplot(data = qc_data %>% 
+         filter(between(Date, beg_date, end_date)) %>% 
+         mutate(Salinity = ifelse(Salinity_final == 1, 
+                                  Salinity, NA)), 
        aes(x = DateTime, 
            y = Salinity)) + 
   theme_bw() + 
@@ -104,8 +115,9 @@ ggsave(here("figs", "coupeville",
        height = 4, width = 6)
 
 #### Mooring - nitrate over time ####
-ggplot(data = raw_data %>% 
-         filter(Year <= yoi), 
+ggplot(data = qc_data %>% 
+         filter(Year <= yoi) %>% 
+         mutate(NO23 = ifelse(NO23_final == 1, NO23, NA)), 
        aes(x = FakeDateTime, 
            y = NO23, 
            color = Year == yoi)) + 
@@ -125,8 +137,9 @@ ggsave(here("figs", "coupeville", paste0("NO23_", yoi, ".png")),
        height = 4, width = 6)
 
 #### Mooring - DO over time ####
-ggplot(data = raw_data %>% 
-         filter(Year <= yoi), 
+ggplot(data = qc_data %>% 
+         filter(Year <= yoi) %>% 
+         mutate(Oxygen = ifelse(Oxygen_final == 1, Oxygen, NA)), 
        aes(x = FakeDateTime, 
            y = Oxygen, 
            color = Year == yoi)) + 
@@ -148,8 +161,9 @@ ggsave(here("figs", "coupeville", paste0("DO_", yoi, ".png")),
 #### Mooring - DO short period ####
 beg_date <- as.Date("2024-11-01")
 end_date <- as.Date("2024-11-30")
-ggplot(data = raw_data %>% 
-         filter(between(Date, beg_date, end_date)), 
+ggplot(data = qc_data %>% 
+         filter(between(Date, beg_date, end_date)) %>% 
+         mutate(Oxygen = ifelse(Oxygen_final == 1, Oxygen, NA)), 
        aes(x = DateTime, 
            y = Oxygen)) + 
   theme_bw() + 
@@ -160,15 +174,16 @@ ggplot(data = raw_data %>%
        color = "", 
        title = "Coupeville") + 
   scale_x_datetime(date_breaks = "1 week", 
-                   date_labels = "%m-%d-%y") + 
-  lims(y = c(0, NA))
+                   date_labels = "%m-%d-%y")
 ggsave(here("figs", "coupeville", 
             paste0("DO_", beg_date, "_", end_date, ".png")), 
        height = 4, width = 6)
 
 #### Mooring - chl over time ####
-ggplot(data = raw_data %>% 
-         filter(Year <= yoi), 
+ggplot(data = qc_data %>% 
+         filter(Year <= yoi) %>% 
+         mutate(Chlorophyll = ifelse(Chlorophyll_final == 1, 
+                                     Chlorophyll, NA)), 
        aes(x = FakeDateTime, 
            y = Chlorophyll, 
            color = Year == yoi)) + 
@@ -182,14 +197,14 @@ ggplot(data = raw_data %>%
   scale_color_manual(values = c("TRUE" = "black", 
                                 "FALSE" = "gray")) + 
   scale_x_datetime(date_breaks = "1 month", 
-                   date_labels = "%b") + 
-  lims(y = c(0, 100))
+                   date_labels = "%b")
 ggsave(here("figs", "coupeville", paste0("chl_", yoi, ".png")), 
        height = 4, width = 6)
 
 #### Mooring - turbidity over time ####
-ggplot(data = raw_data %>% 
-         filter(Turbidity < 100), 
+ggplot(data = qc_data %>% 
+         mutate(Turbidity = ifelse(Turbidity_final == 1, Turbidity, NA)) %>% 
+         filter(Year <= yoi), 
        aes(x = FakeDateTime, 
            y = Turbidity, 
            color = Year == yoi)) + 
@@ -199,7 +214,6 @@ ggplot(data = raw_data %>%
   labs(x = "", 
        y = "Turbidity (NTU)", 
        color = "") + 
-  scale_y_continuous(limits = c(0, NA)) + 
   scale_color_manual(values = c("TRUE" = "black", 
                                 "FALSE" = "gray"))
 ggsave(here("figs", "coupeville", paste0("turb_", yoi, ".png")), 
@@ -208,7 +222,8 @@ ggsave(here("figs", "coupeville", paste0("turb_", yoi, ".png")),
 #### Mooring - pressure and tide height over a month ####
 ggplot(data = comb_data %>% 
          filter(Year == yoi, 
-                Month == 9), 
+                Month == 9) %>% 
+         mutate(Pressure = ifelse(Pressure_final == 1, Pressure, NA)), 
        aes(x = DateTime)) + 
   theme_bw() + 
   geom_point(aes(y = Pressure)) + 
@@ -219,7 +234,8 @@ ggplot(data = comb_data %>%
 #### Mooring - pressure vs tide height over a month ####
 ggplot(data = comb_data %>% 
          filter(Year == yoi, 
-                Month == 9), 
+                Month == 9) %>% 
+         mutate(Pressure = ifelse(Pressure_final == 1, Pressure, NA)), 
        aes(x = Pressure, 
            y = TideHeight)) + 
   theme_bw() + 
@@ -230,9 +246,10 @@ ggsave(here("figs", "coupeville", paste0("pressure_vs_tide_", yoi, ".png")),
        height = 4, width = 6)
 
 #### Mooring - pressure vs. salinity ####
-ggplot(data = raw_data %>% 
-         filter(!Pressure < 0, 
-                Year == yoi), 
+ggplot(data = qc_data %>% 
+         filter(Year == yoi) %>% 
+         mutate(Pressure = ifelse(Pressure_final == 1, Pressure, NA), 
+                Salinity = ifelse(Salinity_final == 1, Salinity, NA)), 
        aes(x = Pressure, 
            y = Salinity)) + 
   theme_bw() + 
