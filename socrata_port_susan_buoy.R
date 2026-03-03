f1c68f9cb3b06053c68dccb8eb1d077ee595c6be
Martin <tsparksmartin@gmail.com>
2026-03-03 10:57:26 -0800

    Adjusted Port Susan Socrata time parsing - fixed midnight bug

--- socrata_port_susan_buoy.R
+++ socrata_port_susan_buoy.R
@@ -56,7 +56,14 @@ process_socrata_psusan <- function(start_date, start_time,
                                  tz = "UTC"), 
            NewDateTime = with_tz(DateTime, tzone = "Etc/GMT+8"), 
            Date = as.Date(str_sub(NewDateTime, 1, 10)), 
-           Time = parse_time(str_sub(NewDateTime, 12, 19))) %>% 
+           Time = parse_time(
+             if_else(
+               str_sub(NewDateTime, 12, 19) == "", 
+               "00:00:00", 
+               str_sub(NewDateTime, 12, 19)
+             )
+           )
+    ) %>% 
     select(-DateTime, -NewDateTime) %>% 
     filter(Date > start_date | 
              Date == start_date & 
