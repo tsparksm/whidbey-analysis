dd5f42bbd9262d39d033320e5395d5cca08766bc
Martin <tsparksmartin@gmail.com>
2026-03-03 10:57:15 -0800

    Adjusted Penn Cove surface Socrata time parsing - fixed midnight bug

--- socrata_penn_cove_buoy_surface.R
+++ socrata_penn_cove_buoy_surface.R
@@ -58,11 +58,13 @@ process_socrata_penncovesurf <- function(start_date,
                                  tz = "UTC"), 
            NewDateTime = with_tz(DateTime, tzone = "Etc/GMT+8"), 
            Date = as.Date(str_sub(NewDateTime, 1, 10)), 
-           Time = parse_time(if_else(
-             str_sub(NewDateTime, 12, 19) == "", 
-             "00:00:00", 
-             str_sub(NewDateTime, 12, 19)
-           )), 
+           Time = parse_time(
+             if_else(
+               str_sub(NewDateTime, 12, 19) == "", 
+               "00:00:00", 
+               str_sub(NewDateTime, 12, 19)
+             )
+           ), 
            `SDI-12Sensor(M_Parameter1)(Inactive-1)` = NA, 
            `SDI-12Sensor(M_Parameter2)(Inactive-2)` = NA, 
            `SDI-12Sensor(M_Parameter3)(Inactive-3)` = NA) %>% 
